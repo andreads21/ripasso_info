c08322cd59f6b194bee94c6a7a516aadd42f2dc4
andreads21 <andrea.de.simone.99@gmail.com>
2023-06-23 21:22:09 +0200

    correzione

--- codice/esame_stabilimento_balneare.py
+++ codice/esame_stabilimento_balneare.py
@@ -3,14 +3,17 @@ f=open('stabilimento.txt','r', encoding='utf-8')
 file=[]
 for fila in f:
     fila=fila.strip().split(', ')
-    d={'o':fila[0], 'co':fila[1], 's':fila[2],'cs':fila[3]}
+    """ io le conversioni le metterei qui """
+    d={'o':int(fila[0]), 'co':int(fila[1]), 's':int(fila[2]),'cs':int(fila[3])}
     file.append(d)
-print(file)
+# print(file)
 
 
 f1=open('ingressi-uscite.txt','r', encoding='utf-8' )
 
 dict={}
+"""devi memorizzare anche l'incasso"""
+incasso = 0
 for persona in f1:
     persona=persona.strip().split(', ')
     if len(persona)> 1: #la persona sta entrando
@@ -19,27 +22,50 @@ for persona in f1:
         nsedie=persona[2]
         buget=persona[3]
         trovata=False
-        if trovata == False: #se non ho trovato la fila
-            for fila in range(len(file)):
-                if int(file[fila]['o'])>= int(nomb) and int(file[fila]['s'])>= int(nsedie) and ((int(nomb)*int(file[fila]['co']))+(int(nsedie)*int(file[fila]['cs'])))<= int(buget):
-                    dict[nome] = fila + 1
+        for fila in range(len(file)):
+            """occhio a dove piazzi questo if, come l'avevi messo tu all'inizio entrava e non lo ripassava mai """
+            if trovata == False:  # se non ho trovato la fila
+                if not trovata and file[fila]['o']>= int(nomb) and file[fila]['s']>= int(nsedie) and ((int(nomb)*file[fila]['co'])+(int(nsedie)*file[fila]['cs']))<= int(buget):
+                    """ devi memorizzare anche gli ombrelloni e le sedie occupate altrimenti quando si libera il posto
+                        non sai quante rimetterne libere"""
+                    dict[nome] = {'f':int(fila), 'o':int(nomb), 's':int(nsedie)}
                     trovata=True #se la trovo tolgo le sedie e gli ombrellloni occuoati e non entro più nel ciclo
-                    if trovata and int(file[fila]['o'])!=0 and int(file[fila]['s'])!=0:
-                            file[fila]['o']=int(file[fila]['o']) - int(nomb)
-                            file[fila]['s']=int(file[fila]['s']) - int(nsedie)
+                    """ mi sembra inutile questo controllo"""
+                    # if trovata and int(file[fila]['o'])!=0 and int(file[fila]['s'])!=0:
+                    file[fila]['o']=file[fila]['o'] - int(nomb)
+                    file[fila]['s']=file[fila]['s'] - int(nsedie)
+                    incasso += int(nomb)*file[fila]['co']+(int(nsedie)*file[fila]['cs'])
 
-                else:
-                    trovata=False
+                    print(f'{nome} è in fila {fila + 1}')
+
+                """ puoi evitare di mettere questa parte"""
+                # else:
+                #     trovata=False
+
+        """ tutte le file sono state controllate e non è stato trovato posto """
+        if not trovata:
+            print(f'Il cliente {nome} non ha trovato posto')
+
     else:#la persona sta uscendo
         nomee=persona[0]
-        file[fila]['o'] = int(file[fila]['o']) + int(nomb)
-        file[fila]['s'] = int(file[fila]['s']) + int(nsedie)
+        """ occhio che fila qui non è definito (lo è solo nel for a linea 24). fai attenzione a queste cose l'IDE 
+            comunque ti avvisa """
+        # file[fila]['o'] = int(file[fila]['o']) + int(nomb)
+        # file[fila]['s'] = int(file[fila]['s']) + int(nsedie)
+        file[dict[nomee]['f']]['o'] += dict[nomee]['o']
+        file[dict[nomee]['f']]['s'] += dict[nomee]['s']
 
+        print(f'{nomee} è uscito')
 
-print(dict)
-for persona in f1:
-    persona = persona.strip().split(', ')
-    if len(persona)> 1:
-        print(f'{persona[0]} è in fila {dict[persona[0]]}')
-    else:
-        print(f'{persona[0]} è uscito')
+print(f"L'incasso della giornata è {incasso}")
+"""
+il dizionario 'dict' è solo usato per tenere traccia delle persone presenti in quel momento nello stabilimento non si
+può usare alla fine per stampare le entrate/uscite in questo caso è molto più semplice farlo all'interno del programma
+"""
+# print(dict)
+# for persona in f1:
+#     persona = persona.strip().split(', ')
+#     if len(persona)> 1:
+#         print(f'{persona[0]} è in fila {dict[persona[0]]}')
+#     else:
+#         print(f'{persona[0]} è uscito')
